fba832c9297df3d1ddf449b6d549e981fa9059ec
Subhitsha26 <79441368+Subhitsha26@users.noreply.github.com>
2021-06-07 11:44:02 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+      Hi, I’m Davis Subhitsha and in this blog, I’m going to share my experience of online learning so hopefully others 
+      have a better idea of what this next year could be like.My course continued to run online from the day of lockdown. 
+      It was an interesting and different with a little trial and error, ended up being very successful.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
